92937a6336212a03488f72c614a30530609b0bf8
sheik-mostafizur <sheikmostafizur@gmail.com>
2024-07-11 07:15:02 +0600

    remove json

--- src/config/database.ts
+++ src/config/database.ts
@@ -16,22 +16,22 @@ interface DbConfigs {
 const config: DbConfigs = {
   development: {
     username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || 'password',
-    database: process.env.DB_NAME || 'database_development',
+    password: process.env.DB_PASS || '',
+    database: process.env.DB_NAME || 'express_db_dev',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql',
   },
   test: {
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASS || 'password',
-    database: process.env.DB_NAME || 'database_test',
+    database: process.env.DB_NAME || 'express_db_test',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql',
   },
   production: {
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASS || 'password',
-    database: process.env.DB_NAME || 'database_production',
+    database: process.env.DB_NAME || 'express_db_production',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql',
     use_env_variable: 'DATABASE_URL',
